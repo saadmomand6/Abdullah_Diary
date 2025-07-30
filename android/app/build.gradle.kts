36dcf9542e5682378faf49bc4b98b27a45a26592
harisjanu <crystaltechqta@gmail.com>
2025-07-31 01:29:58 +0500

    Chnages Done

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.abdullah_diary"
+    namespace = "com.abdullah.diary"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.abdullah_diary"
+        applicationId = "com.abdullah.diary"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
