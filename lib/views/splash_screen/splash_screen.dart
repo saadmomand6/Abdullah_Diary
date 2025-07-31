b08d9ecd863044620017242eb1ddc5d72b47bce3
harisjanu <crystaltechqta@gmail.com>
2025-07-31 18:47:10 +0500

    app icon and native screen changes done

--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:abdullah_diary/widgets/navbar/navbar.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 import 'dart:async';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -9,18 +10,22 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>  with SingleTickerProviderStateMixin{
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
   late final AnimationController _controller = AnimationController(
     duration: const Duration(seconds: 5),
     vsync: this,
   )..repeat();
+
   @override
   void initState() {
     super.initState();
     Timer(
-        const Duration(seconds: 5),
-        () => Navigator.push(
-            context, MaterialPageRoute(builder: (_) => const NavBar())));
+      const Duration(seconds: 5),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const NavBar()),
+      ),
+    );
   }
 
   @override
@@ -28,6 +33,7 @@ class _SplashScreenState extends State<SplashScreen>  with SingleTickerProviderS
     _controller.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,34 +41,30 @@ class _SplashScreenState extends State<SplashScreen>  with SingleTickerProviderS
         constraints: const BoxConstraints.expand(),
         decoration: const BoxDecoration(color: Colors.yellow),
         child: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AnimatedBuilder(
-              animation: _controller,
-              builder: (BuildContext context, child) {
-                return Transform.scale(
-                  scale: _controller.value * 1.0 * math.pi,
-                  child: child,
-                );
-              },
-              child: Container(
-                color: Colors.transparent,
-                height: 200,
-                width: 200,
-                child: Center(
-                  child: Text("Company logo")
-                  // Image(
-                  //   image: const AssetImage('assets/logo.png'),
-                  //   height: MediaQuery.of(context).size.height / 4,
-                  //   width: MediaQuery.of(context).size.width / 4,
-                  // ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AnimatedBuilder(
+                animation: _controller,
+                builder: (BuildContext context, child) {
+                  return Transform.scale(
+                    scale: 0.8 + 0.2 * math.sin(_controller.value * 2 * math.pi),
+                    child: child,
+                  );
+                },
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset(
+                    'assets/logo.png',
+                    fit: BoxFit.contain,
+                  ),
                 ),
               ),
-            ),
-          ],
-        )),
+            ],
+          ),
+        ),
       ),
     );
   }
